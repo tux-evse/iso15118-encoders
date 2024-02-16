dab8aaa32347596ba4748000ffe3caa4ac1521c7
José Bollo <jose.bollo@iot.bzh>
2024-02-16 17:38:17 +0100

    Add check of PaymentDetailsReq

--- src/encoders/utils/iso-2-utils.c
+++ src/encoders/utils/iso-2-utils.c
@@ -4,20 +4,36 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <memory.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ctype.h>
+
+#include <gnutls/x509.h>
 
 #define MAX_EXI_SIZE        8192
 #define DIGEST_ALGO         GNUTLS_DIG_SHA256
 #define DIGEST_MAX_SIZE     32
 #define CHALLENGE_SIZE      16
 #define SIGNING_ALGO        GNUTLS_SIGN_ECDSA_SECP256R1_SHA256
-
+#define MAX_NR_SUB_CERT     iso2_certificateType_4_ARRAY_SIZE
+#define MAX_CERT_SIZE       iso2_certificateType_BYTES_SIZE    
+#define MAX_EMAID_SIZE      iso2_eMAID_CHARACTER_SIZE
+/**
+ * Compute the hash of the given fragment for the given algo.
+ * Store the result in digest that is of size szdigest
+ * The length of the computed hash is stored in *dlen
+ * Returned status is one of:
+ *  - ISO2_UTILS_ERROR_ENCODING
+ *  - ISO2_UTILS_ERROR_INTERNAL1
+ *  - ISO2_UTILS_DONE
+ */
 iso2_utils_status_t
 iso2_utils_get_fragment_digest(
-        const struct iso2_exiFragment *fragment,
-        gnutls_digest_algorithm_t dalgo,
-        uint8_t *digest,
-        unsigned szdigest,
-        unsigned *dlen
+    const struct iso2_exiFragment *fragment,
+    gnutls_digest_algorithm_t dalgo,
+    uint8_t *digest,
+    unsigned szdigest,
+    unsigned *dlen
 ) {
     unsigned char buffer[MAX_EXI_SIZE];
     exi_bitstream_t stream;
@@ -42,13 +58,23 @@ iso2_utils_get_fragment_digest(
     return ISO2_UTILS_DONE;
 }
 
+/**
+ * Compute the hash of the info items of the signature
+ * That hash is the data that is then signed.
+ * Store the result in digest that is of size szdigest
+ * The length of the computed hash is stored in *dlen
+ * Returned status is one of:
+ *  - ISO2_UTILS_ERROR_ENCODING
+ *  - ISO2_UTILS_ERROR_INTERNAL1
+ *  - ISO2_UTILS_DONE
+ */
 iso2_utils_status_t
 iso2_utils_get_signature_digest(
-        const struct iso2_SignatureType *signature,
-        gnutls_digest_algorithm_t dalgo,
-        uint8_t *digest,
-        unsigned szdigest,
-        unsigned *dlen
+    const struct iso2_SignatureType *signature,
+    gnutls_digest_algorithm_t dalgo,
+    uint8_t *digest,
+    unsigned szdigest,
+    unsigned *dlen
 ) {
     struct iso2_exiFragment sig;
 
@@ -60,17 +86,24 @@ iso2_utils_get_signature_digest(
 }
 
 /**
-* Checks that the reference digest matches the digest of the fragment
-*
-* CAUTION! No provision is made to check that the reference is done with
-*   - canonisation = http://www.w3.org/TR/canonical-exi/
-*   - transform = http://www.w3.org/TR/canonical-exi/
-*   - digest method = http://www.w3.org/2001/04/xmlenc#sha256
-*/
+ * Checks that the reference digest matches the digest of the fragment
+ *
+ * CAUTION! No provision is made to check that the reference is done with
+ *   - canonisation = http://www.w3.org/TR/canonical-exi/
+ *   - transform = http://www.w3.org/TR/canonical-exi/
+ *   - digest method = http://www.w3.org/2001/04/xmlenc#sha256
+ *
+ * Returned status is one of:
+ *  - ISO2_UTILS_ERROR_ENCODING
+ *  - ISO2_UTILS_ERROR_INTERNAL1
+ *  - ISO2_UTILS_ERROR_DIGEST_LENGTH
+ *  - ISO2_UTILS_ERROR_DIGEST_MISMATCH
+ *  - ISO2_UTILS_DONE
+ */
 iso2_utils_status_t
 iso2_utils_check_fragment_digest(
-        const struct iso2_ReferenceType *reference,
-        const struct iso2_exiFragment *fragment
+    const struct iso2_ReferenceType *reference,
+    const struct iso2_exiFragment *fragment
 ) {
     unsigned dlen;
     uint8_t digest[DIGEST_MAX_SIZE];
@@ -90,19 +123,25 @@ iso2_utils_check_fragment_digest(
 }
 
 /**
-* Checks the public key validates the signature items
-*
-* CAUTION! No provision is made to check that the reference is done with
-*   - canonisation = http://www.w3.org/TR/canonical-exi/
-*   - transform = http://www.w3.org/TR/canonical-exi/
-*   - digest method = http://www.w3.org/2001/04/xmlenc#sha256
-*   - signature method = http://www.w3.org/2001/04/xmldsig-more#ecdsa-sha256
-*     ()
-*/
+ * Checks the public key validates the signature items
+ *
+ * CAUTION! No provision is made to check that the reference is done with
+ *   - canonisation = http://www.w3.org/TR/canonical-exi/
+ *   - transform = http://www.w3.org/TR/canonical-exi/
+ *   - digest method = http://www.w3.org/2001/04/xmlenc#sha256
+ *   - signature method = http://www.w3.org/2001/04/xmldsig-more#ecdsa-sha256
+ *     ()
+ *
+ * Returned status is one of:
+ *  - ISO2_UTILS_ERROR_ENCODING
+ *  - ISO2_UTILS_ERROR_INTERNAL1
+ *  - ISO2_UTILS_ERROR_BAD_SIGNATURE
+ *  - ISO2_UTILS_DONE
+ */
 iso2_utils_status_t
 iso2_utils_check_signature(
-        const struct iso2_SignatureType *signature,
-        gnutls_pubkey_t pubkey
+    const struct iso2_SignatureType *signature,
+    gnutls_pubkey_t pubkey
 ) {
     int rc;
     unsigned dlen;
@@ -126,20 +165,29 @@ iso2_utils_check_signature(
 }
 
 /**
-* Checks with  the public key that the signature validates the given fragment
-*
-* CAUTION! No provision is made to check that the reference is done with
-*   - canonisation = http://www.w3.org/TR/canonical-exi/
-*   - transform = http://www.w3.org/TR/canonical-exi/
-*   - digest method = http://www.w3.org/2001/04/xmlenc#sha256
-*   - signature method = http://www.w3.org/2001/04/xmldsig-more#ecdsa-sha256
-*     ()
-*/
+ * Checks with  the public key that the signature validates the given fragment
+ *
+ * CAUTION! No provision is made to check that the reference is done with
+ *   - canonisation = http://www.w3.org/TR/canonical-exi/
+ *   - transform = http://www.w3.org/TR/canonical-exi/
+ *   - digest method = http://www.w3.org/2001/04/xmlenc#sha256
+ *   - signature method = http://www.w3.org/2001/04/xmldsig-more#ecdsa-sha256
+ *     ()
+ *
+ * Returned status is one of:
+ *  - ISO2_UTILS_ERROR_NOT_SINGLE_SIGNED
+ *  - ISO2_UTILS_ERROR_ENCODING
+ *  - ISO2_UTILS_ERROR_INTERNAL1
+ *  - ISO2_UTILS_ERROR_BAD_SIGNATURE
+ *  - ISO2_UTILS_ERROR_DIGEST_LENGTH
+ *  - ISO2_UTILS_ERROR_DIGEST_MISMATCH
+ *  - ISO2_UTILS_DONE
+ */
 iso2_utils_status_t
 iso2_utils_check_single_fragment_signature(
-        const struct iso2_SignatureType *signature,
-        const struct iso2_exiFragment *fragment,
-        gnutls_pubkey_t pubkey
+    const struct iso2_SignatureType *signature,
+    const struct iso2_exiFragment *fragment,
+    gnutls_pubkey_t pubkey
 ) {
     int rc;
     unsigned dlen;
@@ -148,7 +196,7 @@ iso2_utils_check_single_fragment_signature(
 
     /* check single reference */
     if (signature->SignedInfo.Reference.arrayLen != 1)
-	return ISO2_UTILS_ERROR_NOT_SINGLE_SIGNED;
+    return ISO2_UTILS_ERROR_NOT_SINGLE_SIGNED;
 
     /* check validity of fragment's hash */
     rc = iso2_utils_check_fragment_digest(&signature->SignedInfo.Reference.array[0], fragment);
@@ -159,57 +207,343 @@ iso2_utils_check_single_fragment_signature(
     return iso2_utils_check_signature(signature, pubkey);
 }
 
+/**
+ * Checks that the AuthorisationReq message has a challenge signed
+ * by the given key
+ *
+ * Returned status is one of:
+ *  - ISO2_UTILS_ERROR_NOT_AUTHORIZATION_REQ
+ *  - ISO2_UTILS_ERROR_NO_SIGNATURE
+ *  - ISO2_UTILS_ERROR_NO_CHALLENGE
+ *  - ISO2_UTILS_ERROR_CHALLENGE_SIZE
+ *  - ISO2_UTILS_ERROR_CHALLENGE_MISMATCH
+ *  - ISO2_UTILS_ERROR_NOT_SINGLE_SIGNED
+ *  - ISO2_UTILS_ERROR_ENCODING
+ *  - ISO2_UTILS_ERROR_INTERNAL1
+ *  - ISO2_UTILS_ERROR_BAD_SIGNATURE
+ *  - ISO2_UTILS_ERROR_DIGEST_LENGTH
+ *  - ISO2_UTILS_ERROR_DIGEST_MISMATCH
+ *  - ISO2_UTILS_DONE
+ */
 iso2_utils_status_t
 iso2_utils_check_authorization_req_signature(
-	const struct iso2_V2G_Message *message,
-	const uint8_t *challenge,
-        gnutls_pubkey_t pubkey
+    const struct iso2_V2G_Message *message,
+    const uint8_t *challenge,
+    gnutls_pubkey_t pubkey
 ) {
-	struct iso2_exiFragment fragment;
+    struct iso2_exiFragment fragment;
 
-	/* validate the request */
-	if (message->Body.AuthorizationReq_isUsed == 0)
-		return ISO2_UTILS_ERROR_NOT_AUTHORIZATION_REQ;
-	if (message->Header.Signature_isUsed == 0)
-		return ISO2_UTILS_ERROR_NO_SIGNATURE;
+    /* validate the request */
+    if (message->Body.AuthorizationReq_isUsed == 0)
+        return ISO2_UTILS_ERROR_NOT_AUTHORIZATION_REQ;
+    if (message->Header.Signature_isUsed == 0)
+        return ISO2_UTILS_ERROR_NO_SIGNATURE;
 
-	/* validate the challenge */
-	if (message->Body.AuthorizationReq.GenChallenge_isUsed == 0)
-		return ISO2_UTILS_ERROR_NO_CHALLENGE;
-	if (message->Body.AuthorizationReq.GenChallenge.bytesLen != CHALLENGE_SIZE)
-		return ISO2_UTILS_ERROR_CHALLENGE_SIZE;
-	if (memcmp(message->Body.AuthorizationReq.GenChallenge.bytes, challenge, CHALLENGE_SIZE))
-		return ISO2_UTILS_ERROR_CHALLENGE_MISMATCH;
+    /* validate the challenge */
+    if (message->Body.AuthorizationReq.GenChallenge_isUsed == 0)
+        return ISO2_UTILS_ERROR_NO_CHALLENGE;
+    if (message->Body.AuthorizationReq.GenChallenge.bytesLen != CHALLENGE_SIZE)
+        return ISO2_UTILS_ERROR_CHALLENGE_SIZE;
+    if (memcmp(message->Body.AuthorizationReq.GenChallenge.bytes, challenge, CHALLENGE_SIZE))
+        return ISO2_UTILS_ERROR_CHALLENGE_MISMATCH;
 
-	/* initiate the fragment to check */
-	init_iso2_exiFragment(&fragment);
-	fragment.AuthorizationReq_isUsed = 1u;
-	memcpy(&fragment.AuthorizationReq, &message->Body.AuthorizationReq, sizeof fragment.AuthorizationReq);
+    /* initiate the fragment to check */
+    init_iso2_exiFragment(&fragment);
+    fragment.AuthorizationReq_isUsed = 1u;
+    memcpy(&fragment.AuthorizationReq, &message->Body.AuthorizationReq, sizeof fragment.AuthorizationReq);
 
-	/* check the fragment */
-	return iso2_utils_check_single_fragment_signature(&message->Header.Signature, &fragment, pubkey);
+    /* check the fragment */
+    return iso2_utils_check_single_fragment_signature(&message->Header.Signature, &fragment, pubkey);
 }
 
-
+/**
+ * Checks that the MeteringReceiptReq message is signed
+ * by the given key
+ *
+ * Returned status is one of:
+ *  - ISO2_UTILS_ERROR_NOT_METERING_RECEIPT_REQ
+ *  - ISO2_UTILS_ERROR_NO_SIGNATURE
+ *  - ISO2_UTILS_ERROR_NOT_SINGLE_SIGNED
+ *  - ISO2_UTILS_ERROR_ENCODING
+ *  - ISO2_UTILS_ERROR_INTERNAL1
+ *  - ISO2_UTILS_ERROR_BAD_SIGNATURE
+ *  - ISO2_UTILS_ERROR_DIGEST_LENGTH
+ *  - ISO2_UTILS_ERROR_DIGEST_MISMATCH
+ *  - ISO2_UTILS_DONE
+ */
 iso2_utils_status_t
 iso2_utils_check_metering_receipt_req_signature(
-	const struct iso2_V2G_Message *message,
-	const uint8_t *challenge,
-        gnutls_pubkey_t pubkey
+    const struct iso2_V2G_Message *message,
+    gnutls_pubkey_t pubkey
 ) {
-	struct iso2_exiFragment fragment;
+    struct iso2_exiFragment fragment;
 
-	/* validate the request */
-	if (message->Body.MeteringReceiptReq_isUsed == 0)
-		return ISO2_UTILS_ERROR_NOT_METERING_RECEIPT_REQ;
-	if (message->Header.Signature_isUsed == 0)
-		return ISO2_UTILS_ERROR_NO_SIGNATURE;
+    /* validate the request */
+    if (message->Body.MeteringReceiptReq_isUsed == 0)
+        return ISO2_UTILS_ERROR_NOT_METERING_RECEIPT_REQ;
+    if (message->Header.Signature_isUsed == 0)
+        return ISO2_UTILS_ERROR_NO_SIGNATURE;
 
-	/* initiate the fragment to check */
-	init_iso2_exiFragment(&fragment);
-	fragment.MeteringReceiptReq_isUsed = 1u;
-	memcpy(&fragment.MeteringReceiptReq, &message->Body.MeteringReceiptReq, sizeof fragment.MeteringReceiptReq);
+    /* initiate the fragment to check */
+    init_iso2_exiFragment(&fragment);
+    fragment.MeteringReceiptReq_isUsed = 1u;
+    memcpy(&fragment.MeteringReceiptReq, &message->Body.MeteringReceiptReq, sizeof fragment.MeteringReceiptReq);
 
-	/* check the fragment */
-	return iso2_utils_check_single_fragment_signature(&message->Header.Signature, &fragment, pubkey);
+    /* check the fragment */
+    return iso2_utils_check_single_fragment_signature(&message->Header.Signature, &fragment, pubkey);
 }
+
+/**
+ * Loads the certificate at the given path in cert
+ *
+ * Returned status is one of:
+ *  - ISO2_UTILS_ERROR_ROOTCERT_OPEN
+ *  - ISO2_UTILS_ERROR_ROOTCERT_READ
+ *  - ISO2_UTILS_ERROR_ROOTCERT_OVERFLOW
+ *  - ISO2_UTILS_ERROR_INTERNAL5
+ *  - ISO2_UTILS_ERROR_ROOTCERT_IMPORT
+ *  - ISO2_UTILS_DONE
+ */
+iso2_utils_status_t
+load_root_cert(
+    const char *path,
+    gnutls_x509_crt_t *cert
+) {
+    int fd, rc;
+    ssize_t rsz;
+    gnutls_datum_t data;
+    uint8_t buffer[MAX_CERT_SIZE + 1];
+
+    /* read the file */
+    fd = open(path, O_RDONLY);
+    if (fd < 0)
+	    return ISO2_UTILS_ERROR_ROOTCERT_OPEN;
+    rsz = read(fd, buffer, sizeof buffer);
+    close(fd);
+    if (rsz < 0)
+        return ISO2_UTILS_ERROR_ROOTCERT_READ;
+    if ((size_t)rsz > MAX_CERT_SIZE)
+        return ISO2_UTILS_ERROR_ROOTCERT_OVERFLOW;
+
+    /* make the certificate */
+    rc = gnutls_x509_crt_init(cert);
+    if (rc != GNUTLS_E_SUCCESS) {
+        return ISO2_UTILS_ERROR_INTERNAL5;
+    }
+    data.data = buffer;
+    data.size = (size_t)rsz;
+    rc = gnutls_x509_crt_import(*cert, &data, GNUTLS_X509_FMT_DER);
+    if (rc != GNUTLS_E_SUCCESS) {
+        gnutls_x509_crt_deinit(*cert);
+        return ISO2_UTILS_ERROR_ROOTCERT_IMPORT;
+    }
+    return ISO2_UTILS_DONE;
+}
+
+/**
+ * Compares the 2 given EMAID and returns 0 if they are not equals
+ * or of length null or return the length of the EMAID without decoration.
+ * Decoration is made of dashes separating groups.
+ */
+unsigned
+compare_emaid(
+    const uint8_t *buf1,
+    unsigned len1,
+    const uint8_t *buf2,
+    unsigned len2
+) {
+    unsigned len = 0, idx1 = 0, idx2 = 0;
+    while (idx1 < len1 && idx2 < len2) {
+        if (buf1[idx1] == '-')
+            idx1++;
+        else if (buf2[idx2] == '-')
+            idx2++;
+        else if (toupper(buf1[idx1]) != toupper(buf2[idx2]))
+            return 0;
+        else {
+            idx1++;
+            idx2++;
+            len++;
+        }
+    }
+    while (idx1 < len1 && buf1[idx1] == '-')
+        idx1++;
+    while (idx2 < len2 && buf2[idx2] == '-')
+        idx2++;
+    return idx1 == len1 && idx2 == len2 ? len : 0;
+}
+
+/**
+ * Check the PaymentDetailsReq in its consistency, its link to the
+ * authority if root_cert_path != NULL and extracts the public key
+ * if pubkey it not NULL
+ *
+ * Returned status is one of:
+ *  - ISO2_UTILS_ERROR_NOT_PAYEMENT_DETAIL_REQ
+ *  - ISO2_UTILS_ERROR_CERT_IMPORT
+ *  - ISO2_UTILS_ERROR_SUBJECT_CN
+ *  - ISO2_UTILS_ERROR_EMAID_MISMATCH
+ *  - ISO2_UTILS_ERROR_TOO_MANY_CERT
+ *  - ISO2_UTILS_ERROR_CERT_IMPORT
+ *  - ISO2_UTILS_ERROR_INVALID_CERT
+ *  - ISO2_UTILS_ERROR_ROOTCERT_OPEN
+ *  - ISO2_UTILS_ERROR_ROOTCERT_READ
+ *  - ISO2_UTILS_ERROR_ROOTCERT_OVERFLOW
+ *  - ISO2_UTILS_ERROR_ROOTCERT_IMPORT
+ *  - ISO2_UTILS_ERROR_INTERNAL2
+ *  - ISO2_UTILS_ERROR_INTERNAL3
+ *  - ISO2_UTILS_ERROR_INTERNAL4
+ *  - ISO2_UTILS_ERROR_INTERNAL5
+ *  - ISO2_UTILS_ERROR_INTERNAL6
+ *  - ISO2_UTILS_ERROR_INTERNAL7
+ *  - ISO2_UTILS_ERROR_INTERNAL8
+ *  - ISO2_UTILS_ERROR_INTERNAL9;
+ *  - ISO2_UTILS_DONE
+ */
+iso2_utils_status_t
+iso2_utils_check_payement_details_req(
+    const struct iso2_V2G_Message *message,
+    const char *root_cert_path,
+    gnutls_pubkey_t *pubkey
+) {
+    int rc;
+    unsigned idx, cnt, len, vsts, ncerts = 0;
+    gnutls_x509_crt_t rootcert, certs[MAX_NR_SUB_CERT + 1];
+    gnutls_datum_t data;
+    gnutls_x509_trust_list_t tlist;
+    const struct iso2_PaymentDetailsReqType *msgreq;
+    const struct iso2_CertificateChainType *msgcert;
+    gnutls_x509_dn_t gdn;
+	uint8_t emaid[MAX_EMAID_SIZE];
+    size_t emaidsz;
+
+    /* validate the request */
+    if (message->Body.PaymentDetailsReq_isUsed == 0)
+        return ISO2_UTILS_ERROR_NOT_PAYEMENT_DETAIL_REQ;
+    msgreq = &message->Body.PaymentDetailsReq;
+    msgcert = &msgreq->ContractSignatureCertChain;
+
+    /* import the certificate */
+    rc = gnutls_x509_crt_init(&certs[0]);
+    if (rc != GNUTLS_E_SUCCESS)
+        return ISO2_UTILS_ERROR_INTERNAL2;
+    ncerts = 1;
+    data.data = (void*)msgcert->Certificate.bytes;
+    data.size = msgcert->Certificate.bytesLen;
+    rc = gnutls_x509_crt_import(certs[0], &data, GNUTLS_X509_FMT_DER);
+    if (rc != GNUTLS_E_SUCCESS) {
+        rc = ISO2_UTILS_ERROR_CERT_IMPORT;
+        goto cleanup;
+    }
+
+    /* validate the MEAN */
+    emaidsz = sizeof emaid;
+    rc = gnutls_x509_crt_get_dn_by_oid(certs[0], GNUTLS_OID_X520_COMMON_NAME, 0, 0, emaid, &emaidsz);
+    if (rc < 0) {
+        rc = ISO2_UTILS_ERROR_SUBJECT_CN;
+        goto cleanup;
+    }
+    len = compare_emaid(emaid, (unsigned) emaidsz, msgreq->eMAID.characters, (unsigned)msgreq->eMAID.charactersLen);
+    if (len == 0)  {
+        rc = ISO2_UTILS_ERROR_EMAID_MISMATCH;
+        goto cleanup;
+    }
+
+    /* check the certificates */
+    if (root_cert_path != NULL) {
+        /* import the sub certificates */
+        cnt = msgcert->SubCertificates_isUsed ? msgcert->SubCertificates.Certificate.arrayLen : 0;
+        if (cnt > MAX_NR_SUB_CERT) {
+            rc = ISO2_UTILS_ERROR_TOO_MANY_CERT;
+            goto cleanup;
+        }
+        for (idx = 0 ; idx < cnt ; idx++) {
+            rc = gnutls_x509_crt_init(&certs[ncerts + 1]);
+            if (rc != GNUTLS_E_SUCCESS) {
+                rc = ISO2_UTILS_ERROR_INTERNAL3;
+                goto cleanup;
+            }
+            data.data = (void*)msgcert->SubCertificates.Certificate.array[idx].bytes;
+            data.size = msgcert->SubCertificates.Certificate.array[idx].bytesLen;
+            rc = gnutls_x509_crt_import(certs[++ncerts], &data, GNUTLS_X509_FMT_DER);
+            if (rc != GNUTLS_E_SUCCESS) {
+                rc = ISO2_UTILS_ERROR_CERT_IMPORT;
+                goto cleanup;
+            }
+        }
+
+        /* import the root certificate */
+        rc = load_root_cert(root_cert_path, &rootcert);
+        if (rc != ISO2_UTILS_DONE)
+            goto cleanup;
+
+        /* check the trust chain */
+        rc = gnutls_x509_trust_list_init(&tlist, 1);
+        if (rc != GNUTLS_E_SUCCESS)
+            rc = ISO2_UTILS_ERROR_INTERNAL4;
+        else {
+            rc = gnutls_x509_trust_list_add_cas(tlist, &rootcert, 1, 0);
+            if (rc != GNUTLS_E_SUCCESS)
+                rc = ISO2_UTILS_ERROR_INTERNAL6;
+            else {
+                vsts = 0;
+                rc = gnutls_x509_trust_list_verify_crt(tlist, certs, ncerts, 0, &vsts, NULL);
+                if (rc != GNUTLS_E_SUCCESS)
+                    rc = ISO2_UTILS_ERROR_INTERNAL7;
+                else if (GNUTLS_CERT_INVALID)
+                    rc = ISO2_UTILS_ERROR_INVALID_CERT;
+                else
+                    rc = ISO2_UTILS_DONE;
+            }
+            gnutls_x509_trust_list_deinit(tlist, 1);
+        }
+        gnutls_x509_crt_deinit(rootcert);
+        if (rc != ISO2_UTILS_DONE)
+            goto cleanup;
+    }
+
+    /* extract the public key */
+    if (pubkey != NULL) {
+        rc = gnutls_pubkey_init(pubkey);
+        if (rc != GNUTLS_E_SUCCESS)
+            rc = ISO2_UTILS_ERROR_INTERNAL8;
+        else {
+            rc = gnutls_pubkey_import_x509(*pubkey, certs[0], 0);
+            if (rc == GNUTLS_E_SUCCESS)
+                rc = ISO2_UTILS_DONE;
+            else {
+                gnutls_pubkey_deinit(*pubkey);
+                rc = ISO2_UTILS_ERROR_INTERNAL9;
+            }
+        }
+    }
+
+    /* cleanup */
+cleanup:
+    while (ncerts > 0)
+        gnutls_x509_crt_deinit(certs[--ncerts]);
+    return rc;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/encoders/utils/iso-2-utils.h
+++ src/encoders/utils/iso-2-utils.h
@@ -12,41 +12,68 @@
 #include <gnutls/abstract.h>
 
 typedef enum {
-	ISO2_UTILS_DONE = 0,
+    ISO2_UTILS_DONE = 0,
 
-	ISO2_UTILS_ERROR_ENCODING,
-	ISO2_UTILS_ERROR_INTERNAL1,
-	ISO2_UTILS_ERROR_MAKE_DIGEST,
+    ISO2_UTILS_ERROR_ENCODING,
+    ISO2_UTILS_ERROR_MAKE_DIGEST,
 
-	ISO2_UTILS_ERROR_DIGEST,
-	ISO2_UTILS_ERROR_DIGEST_LENGTH,
-	ISO2_UTILS_ERROR_DIGEST_MISMATCH,
+    ISO2_UTILS_ERROR_DIGEST,
+    ISO2_UTILS_ERROR_DIGEST_LENGTH,
+    ISO2_UTILS_ERROR_DIGEST_MISMATCH,
 
-	ISO2_UTILS_ERROR_NOT_SINGLE_SIGNED,		/* there is more than one signed element */
+    ISO2_UTILS_ERROR_NOT_SINGLE_SIGNED,        /* there is more than one signed element */
 
-	ISO2_UTILS_ERROR_NOT_AUTHORIZATION_REQ,
-	ISO2_UTILS_ERROR_NOT_METERING_RECEIPT_REQ,
-	ISO2_UTILS_ERROR_NO_SIGNATURE,
-	ISO2_UTILS_ERROR_NO_CHALLENGE,
-	ISO2_UTILS_ERROR_CHALLENGE_SIZE,
-	ISO2_UTILS_ERROR_CHALLENGE_MISMATCH,
+    ISO2_UTILS_ERROR_NOT_AUTHORIZATION_REQ,
+    ISO2_UTILS_ERROR_NOT_METERING_RECEIPT_REQ,
+    ISO2_UTILS_ERROR_NOT_PAYEMENT_DETAIL_REQ,
+    ISO2_UTILS_ERROR_NO_SIGNATURE,
+    ISO2_UTILS_ERROR_NO_CHALLENGE,
+    ISO2_UTILS_ERROR_CHALLENGE_SIZE,
+    ISO2_UTILS_ERROR_CHALLENGE_MISMATCH,
 
-	ISO2_UTILS_ERROR_BAD_SIGNATURE
+    ISO2_UTILS_ERROR_BAD_SIGNATURE,
+
+	ISO2_UTILS_ERROR_CERT_IMPORT,
+	ISO2_UTILS_ERROR_SUBCERT_IMPORT,
+	ISO2_UTILS_ERROR_ROOTCERT_OPEN,
+	ISO2_UTILS_ERROR_ROOTCERT_READ,
+	ISO2_UTILS_ERROR_ROOTCERT_OVERFLOW,
+	ISO2_UTILS_ERROR_ROOTCERT_IMPORT,
+	ISO2_UTILS_ERROR_SUBJECT_CN,
+	ISO2_UTILS_ERROR_EMAID_MISMATCH,
+	ISO2_UTILS_ERROR_TOO_MANY_CERT,
+    ISO2_UTILS_ERROR_INVALID_CERT,
+
+    ISO2_UTILS_ERROR_INTERNAL1,
+	ISO2_UTILS_ERROR_INTERNAL2,
+	ISO2_UTILS_ERROR_INTERNAL3,
+	ISO2_UTILS_ERROR_INTERNAL4,
+	ISO2_UTILS_ERROR_INTERNAL5,
+	ISO2_UTILS_ERROR_INTERNAL6,
+    ISO2_UTILS_ERROR_INTERNAL7,
+    ISO2_UTILS_ERROR_INTERNAL8,
+    ISO2_UTILS_ERROR_INTERNAL9
 }
-	iso2_utils_status_t;
+    iso2_utils_status_t;
 
 extern
 iso2_utils_status_t
 iso2_utils_check_authorization_req_signature(
-	const struct iso2_V2G_Message *message,
-	const uint8_t *challenge,
-        gnutls_pubkey_t pubkey
+    const struct iso2_V2G_Message *message,
+    const uint8_t *challenge,
+    gnutls_pubkey_t pubkey
 );
 
 extern
 iso2_utils_status_t
 iso2_utils_check_metering_receipt_req_signature(
-	const struct iso2_V2G_Message *message,
-	const uint8_t *challenge,
-        gnutls_pubkey_t pubkey
+    const struct iso2_V2G_Message *message,
+    gnutls_pubkey_t pubkey
+);
+
+iso2_utils_status_t
+iso2_utils_check_payement_details_req(
+    const struct iso2_V2G_Message *message,
+    const char *root_cert_path,
+    gnutls_pubkey_t *pubkey
 );
